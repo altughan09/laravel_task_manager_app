c058cf232890025ecfc7370061b9c895a00076b3
altughan09 <31768522+altughan09@users.noreply.github.com>
2019-12-22 12:33:35 +0100

    Added route model binding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -13,9 +13,8 @@ class TodosController extends Controller
         return view('todos.index')->with('todos', $todos);
     }
 
-    public function show($todoId)
+    public function show(Todo $todo)
     {
-        $todo = Todo::find($todoId);
         return view('todos.show')->with('todo', $todo);
     }
 
@@ -43,13 +42,12 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function edit($todoId)
+    public function edit(Todo $todo)
     {
-        $todo = Todo::find($todoId);
         return view('todos.edit')->with('todo', $todo);
     }
 
-    public function update($todoId)
+    public function update(Todo $todo)
     {
         $this->validate(request(), [
             'name' => 'required | min:6 | max:40',
@@ -58,7 +56,6 @@ class TodosController extends Controller
         
         $data = request()->all();
 
-        $todo = Todo::find($todoId);
         $todo->name = $data['name'];
         $todo->description = $data['description'];
 
@@ -66,9 +63,8 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function destroy($todoId)
+    public function destroy(Todo $todo)
     {
-        $todo = Todo::find($todoId);
         $todo->delete();
         return redirect('/todos');
     }
